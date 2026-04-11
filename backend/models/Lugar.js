f4c867c675949a8d8d85efa71b59294d6ef319fd
EnriquePozos <enrique.pozosg@uanl.edu.mx>
2026-04-11 02:03:03 -0600

    Modelo de Lugar

---
+++ backend/models/Lugar.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const lugarSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    categoria:{
+      type: String,
+      enum: ['museo', 'parque', 'restaurante', 'cafeteria', 'estadio', 'centro comercial', 'hotel'],
+      required: true  
+    },
+    direccion: {
+        type: String,
+        required: true,
+    },
+    horario: {
+        type: String,
+        required: optional,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+    },
+    url_img: {
+        type: String,
+        required: optional,
+    },
+    url_sitioweb: {
+        type: String,
+        required: optional,
+    },
+    caracteristicas_accesibilidad: [{
+    type: String,
+    enum: [
+      'rampa de acceso', 'elevador', 'baño accesible', 'silla de ruedas',
+      'estacionamiento accesible', 'personal capacitado', 'entrada accesible',
+      'menú braille', 'personal LSM', 'senderos accesibles', 'audiolibros',
+      'señalización braille', 'juegos inclusivos', 'transporte accesible', 'espacio amplio'
+        ],
+    }],
+    estado: {
+        type: String,
+        enum: ['Pendiente', 'Aprobado','Inactivo', 'Rechazado'],
+        default: 'Pendiente',
+    },
+    creadoPor_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true,
+    },
+    
+})
+
+export default mongoose.model('Lugar', lugarSchema);
+    
